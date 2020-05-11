aac12f7fe88d868149153e9d0b155ae88445e64c
zhengwenjun <zhengwenjun@gmail.com>
2020-05-11 23:17:40 +0800

    test2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,7 @@ class MyDocument extends Document {
         <body>
           <script src="/js/jquery.min.js"></script>
           <script type="text/javascript" src="/js/3deye.js"></script>
-          <script type="text/javascript" src="/js/wx.js"></script>
+          {/* <script type="text/javascript" src="/js/wx.js"></script> */}
           <Main />
           <NextScript />
         </body>
